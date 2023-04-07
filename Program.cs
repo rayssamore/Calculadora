f3f92f0cd077247602f6aa5296b0bb54fd97e9ca
unknown <rayssagm_@hotmail.com>
2023-04-07 15:08:03 -0300

    1.0.0

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Numerics;
+
+namespace calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+
+        }
+        static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+            //Console.WriteLine(v1);
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("---------------");
+            float resultado = v1 + v2;
+            Console.WriteLine($"O resultado da Soma é: {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void Subtracao()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+            float resultado = v1 - v2;
+            Console.WriteLine("---------------");
+            Console.WriteLine($"O resultado da Subtração é: {resultado}"); 
+            Console.ReadKey();
+            Menu();
+        }
+        static void Divisao()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+            float resultado = v1 / v2;
+            Console.WriteLine("---------------");
+            Console.WriteLine($"O resultado da Divisão é: {resultado}"); 
+            Console.ReadKey();
+            Menu();
+        }
+        static void Multiplicacao()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+            float resultado = v1 * v2;
+            Console.WriteLine("---------------");
+            Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Sair");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Selecione uma opção: ");
+            short res = short.Parse(Console.ReadLine());
+
+            switch (res){
+                case 1: Soma(); break;
+                case 2: Subtracao() ; break;
+                case 3: Divisao () ; break;
+                case 4: Multiplicacao () ;break;
+                case 5: Sair();break;
+                default: Menu(); break;
+            }
+
+        }
+        static void Sair ()
+        {
+            System.Environment.Exit(0);
+
+        }
+    }
+}
